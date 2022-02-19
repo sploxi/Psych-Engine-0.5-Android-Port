88e4582ea474f09aebc8e4afbdfaab4f0f5af1a4
sploxi <99512815+sploxi@users.noreply.github.com>
2022-02-19 13:03:50 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -68,16 +68,16 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
+	['you can improve', 0.2], //From 0% to 19%
+		['Are you a 6 year old?', 0.4], //From 20% to 39%
+		['Bad as hell', 0.5], //From 40% to 49%
+		['shit', 0.6], //From 50% to 59%
+		['that's ok', 0.69], //From 60% to 68%
+		['ok', 0.7], //69%
 		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['nothing bad', 0.9], //From 80% to 89%
+		['que pro', 1], //From 90% to 99%
+		['OH MY GOD I FOUND A HACKER', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
